9ad60f20b22f53f0decc8eaf0b48eb7a01b3bc82
Ditawidyastuti <32087880+Ditawidyastuti@users.noreply.github.com>
2017-11-20 23:51:38 -0700

    Add files via upload

---
+++ Bentukwajah.java
@@ -0,0 +1,35 @@
+class Bentukwajah{
+    /*Membuat sebuah method respons pada class induk 
+    dimana mengembalikan sebuah  nilai String */
+    public String respons(){
+        return("Perhatikan reaksi wajah saya\n");
+    }
+
+}
+
+/*Membuat class-class lain dengan turunan
+dari class Bentuk wajah*/
+
+class Senyum extends Bentukwajah{
+    public String respons(){
+        return("Senyum karena senang\n");
+    }
+}
+
+class Tertawa extends Bentukwajah{
+    public String respons(){
+        return("Tertawa karena gembira\n");
+    }
+}
+
+class Marah extends Bentukwajah{
+    public String respons(){
+        return("kemarahan disebabkan bertengkar\n");
+    }
+}
+
+class Sedih extends Bentukwajah{
+    public String respons(){
+        return("sedih disebabkan cemburu\n");
+    }
+}
